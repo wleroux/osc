078798aa6f60214118cae2d3ed71173945f05aaf
Wayne Leroux <WayneLeroux@gmail.com>
2015-08-29 15:54:19 -0400

    Added some finalizing touches

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@
   var s = skrollr.init({
     render: function(data) {
     }
-  })
+  });
+
+  var sections = document.getElementsByTagName("section");
+  for (var i = 0; i < sections.length; i ++) {
+    sections[i].style.height = window.innerHeight + "px";
+  }
+
 })();
