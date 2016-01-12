3b85022da521b749f637d0e989f99000cd326c15
Wayne Leroux <WayneLeroux@gmail.com>
2016-01-11 22:47:25 -0500

    Updated the website to be mobile-friendly

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
-(function () {
-  var videos = document.getElementsByTagName("iframe");
-
-  // Initialize aspect ratio
-  for (var i = 0; i < videos.length; i ++) {
-    videos[i].aspectRatio = videos[i].width / videos[i].height;
-  }
-
-  function resizeVideos() {
-    for (i = 0; i < videos.length; i ++) {
-      var height = window.innerHeight * (3 / 4) - 20;
-      var width = height * videos[i].aspectRatio;
-
-      videos[i].style.width = width + "px";
-      videos[i].style.height = height + "px";
-    }
-  }
-
-  document.addEventListener('resize', resizeVideos);
-  resizeVideos();
-
-})();
+(function ($) {
+  $(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+  });
+})(window.jQuery);
